13072dcbacc72c909d18b531e23d0b491d24bb2b
FernandoDev97 <l.fernando.dev@gmail.com>
2021-11-24 19:31:26 -0300

    add dashboard

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,42 @@
 import { ButtonHTMLAttributes, ReactNode } from "react"
+import tw from 'tailwind-styled-components'
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode
+    variat?: 'ghost'
 }
 
-export const Button = ({children, ...props}: Props) => {
+export const Button = ({children, variat, ...props}: Props) => {
+   if(variat === 'ghost'){ 
     return (
-        <button {...props}
-            className=' h-12 max-w-lg
-                        text-white font-bold bg-purple-600 hover:bg-purple-500 
-                        rounded-lg disabled:cursor-not-allowed disabled:bg-gray-300'>
+        <LogoutButton {...props}>
             {children}
-        </button>
+        </LogoutButton>
+    ) 
+   }
+    return (
+        <MainButton {...props}>
+            {children}
+        </MainButton>
     )
 }
+
+export const BaseButton = tw.button`
+    h-12 
+    max-w-lg 
+    px-8
+    text-white 
+    font-bold 
+    rounded-lg 
+    disabled:cursor-not-allowed d
+    isabled:bg-gray-300
+`
+export const MainButton = tw(BaseButton)`
+    bg-purple-600
+    hover:bg-purple-500 
+`
+export const LogoutButton = tw(BaseButton)`
+    bg-transparent
+    bg-red-500
+    hover:bg-red-400
+`
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,20 @@
-export default function Dashboard () {
+import { Button } from "../../components/Button";
+import { TemplateContainer } from "../../components/templates";
+
+export default function Dashboard() {
     return (
         <div>
-            testando
+            <TemplateContainer>
+                <header className='w-full max-w-screen-xl items-center h-1/6 flex justify-between'>
+                    <span className='font-bold text-2xl'>
+                        Finance control
+                    </span>
+                    <nav className='flex gap-4'>
+                        <Button>Adicionar nova Conta</Button>
+                        <Button variat='ghost'>Sair</Button>
+                    </nav>
+                </header>
+            </TemplateContainer>
         </div>
     )
 }
